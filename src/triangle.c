2721ba3f55adf94fb6931e79815503f438f3e5fc
MikeyBoo <5800495+MikeyBoo@users.noreply.github.com>
2020-08-18 22:55:08 -0400

    divide by zero bugfix

--- src/triangle.c
+++ src/triangle.c
@@ -52,13 +52,20 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         int_swap(&x0, &x1);
     }
 
-    //calc Mx,My
-    int My = y1;
-    int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+    if (y1 == y2) {
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else if (y0 == y1) {
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else {
 
-    //draw flat-bottom
-    fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+        //calc Mx,My
+        int My = y1;
+        int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
 
-    //draw flat-top
-    fill_flat_top_triangle( x1, y1, Mx, My, x2, y2, color);
+        //draw flat-bottom
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+
+        //draw flat-top
+        fill_flat_top_triangle( x1, y1, Mx, My, x2, y2, color);
+    }
 }
